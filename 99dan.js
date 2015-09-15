9eb1107ecec1a1314d6802c0279c7c23f37ecfc9
dlgocks1 <cksgodl1@naver.com>
2015-09-15 20:57:20 +0900

    Canvas start

---
+++ 99dan.js
@@ -0,0 +1,14 @@
+﻿function write99dan(_start,_end){
+		var start= _start;
+		var end= _end;
+		for(var i=start;i<end;i++){
+			document.writeln();
+			document.writeln(i+"단");
+			for(var j=1;j<10;j++){
+				document.writeln(i+"x"+j+"="+i*j);
+			}
+			}
+}
+write99dan(2,14);
+
+
